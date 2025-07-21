68a63600725220182faebdd18b6fdc000de00b09
acorkankobra <andy.corkan@kobrabiosolutions.com>
2025-07-21 11:26:30 -0600

    Added demo and tested.

---
+++ CLParserDemo/App.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.CommandLine;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace CLParserDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IServiceProvider _serviceProvider;
+        private IConfiguration _configuration;
+
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the configuration from appsettings.json
+            var configurationBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).
+             AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _configuration = configurationBuilder.Build();
+
+            // Set up the DI service collection.
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var clParser = _serviceProvider.GetRequiredService<CommandLineParser.CLParser>();
+
+            clParser.Add(new Option<string>("--name") { Description = "Your name" });
+
+            Option<bool> headlessOption = new Option<bool>("--headless") { Description = "Run the application in headless mode without GUI." };
+            clParser.Add(headlessOption);
+
+            if(clParser.HasErrors())
+            {
+                MessageBox.Show("Error parsing command line arguments: " + string.Join(", ", clParser.GetErrors()));
+                return;
+            }
+
+            bool b1 = clParser.GetValueForOption<bool>("--headless");
+            bool b2 = clParser.GetValueForOption<bool>(headlessOption);
+            var name = clParser.GetValueForOption<string>("--name");
+
+            if (clParser.GetValueForOption<bool>("--headless"))
+            {
+                Trace.WriteLine("Running in headless mode.");
+            }
+            else
+            {
+                var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+                MainWindow.Show();
+            }
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+            services.AddTransient<MainViewModel>();
+
+            services.AddSingleton<IConfiguration>(_configuration);
+            services.AddSingleton<CommandLineParser.CLParser>();
+        }
+    }
+}
---
+++ CommandLineParser/CLParser.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+
+namespace CommandLineParser
+{
+    public class CLParser
+    {
+        private RootCommand _rootCommand;
+        private string[] _cmdLineArgs;
+        private ParseResult _parseResult;
+        //_cmdLineArgs = Environment.GetCommandLineArgs();
+
+
+        public string AppPath { get; }
+
+        public CLParser()
+        {
+            var args = new List<string>(Environment.GetCommandLineArgs());
+            AppPath = args[0];
+            args.RemoveAt(0); // Remove the application path from the arguments
+            _cmdLineArgs = args.ToArray();
+        }
+
+        private void ParseCommandLineArgs()
+        {
+            if (_parseResult == null)
+            {
+                _parseResult = _rootCommand.Parse(_cmdLineArgs);
+            }
+        }
+        public void Add(Option option)
+        {
+            if(_rootCommand == null)
+            {
+                _rootCommand = new RootCommand();
+            }
+            _rootCommand.Add(option);
+        }
+
+        public bool HasErrors()
+        {
+            return GetErrors().Any();
+        }
+
+        public List<string> GetErrors()
+        {
+            ParseCommandLineArgs();
+            return _parseResult.Errors.Select(e => e.Message).ToList();
+        }
+
+        public T GetValueForOption<T>(Option<T> option)
+        {
+            ParseCommandLineArgs();
+            if (_parseResult.Errors.Any())
+            {
+                throw new ArgumentException("Error parsing command line arguments: " + string.Join(", ", _parseResult.Errors.Select(e => e.Message)));
+            }
+            return _parseResult.GetValue<T>(option);
+        }
+        public T GetValueForOption<T>(string optionName)
+        {
+            ParseCommandLineArgs();
+            if (_parseResult.Errors.Any())
+            {
+                throw new ArgumentException("Error parsing command line arguments: " + string.Join(", ", _parseResult.Errors.Select(e => e.Message)));
+            }
+            var option = _rootCommand.Options.FirstOrDefault(o => o.Name == optionName);
+            return _parseResult.GetValue<T>(option as Option<T>);
+        }
+    }
+}
